29d8302bc7630057b7e93a459495193d9fbe4edb
BengalPirate <114821633+BengalPirate@users.noreply.github.com>
2022-11-17 16:09:12 -0500

    278 version1

--- main.cpp
+++ main.cpp
@@ -1,1 +1,68 @@
-// write your code here
+// Brandon Newton J00610524 11/15/22
+// Due Wednesday Nov 16, 2022
+// P 278
+//Program Sentinel Controlled Loop
+//This program computes and outputs the total number of boxes of
+//cookies sold, the total revenue, and the average number of 
+//boxes sold by each volunteer
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+const string SENTINEL = "-1";
+
+int main()
+{
+  string name;
+  int numOfVolunteers;
+  int numOfBoxesSold;
+  int totalNumOfBoxesSold;
+  double costOfOneBox;
+
+  cout << fixed << showpoint << setprecision(2);
+
+  cout << "Line 14: Enter each volunteer's name and "
+      << "the number of boxes " << endl
+      << "      sold by each volunteer, ending "
+      << "with -1: " << endl;
+
+  totalNumOfBoxesSold = 0;
+  numOfVolunteers = 0;
+
+  cin >> name;
+
+  while (name != SENTINEL)
+    {
+      cin >> numOfBoxesSold;
+      totalNumOfBoxesSold = totalNumOfBoxesSold + numOfBoxesSold;
+
+      numOfVolunteers++;
+      cin >> name;
+    }
+
+  cout << endl;
+
+  cout << "Line 26: The total number of boxes sold: "
+      << totalNumOfBoxesSold << endl;
+
+  cout << "Line 27: Enter the cost of one box: ";
+  cin >> costOfOneBox;
+  cout << endl;
+
+  cout << "Line 30: The total money made by selling "
+      << "cookies: $"
+      << totalNumOfBoxesSold * costOfOneBox << endl;
+
+  if (numOfVolunteers != 0)
+      cout << "Line 32: The average number of "
+        << "boxes sold by each volunteer: "
+        << totalNumOfBoxesSold / numOfVolunteers
+        << endl;
+  else
+      cout << "Line 34: No input." << endl;
+
+  return 0;
+}
